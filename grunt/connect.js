5d86fcdb577338d386ce871c7493f12816783d36
Christopher Parsons <cparsons@habaneroconsulting.com>
2015-08-08 16:02:25 -0700

    Updated developer dependencies

--- grunt/connect.js
+++ grunt/connect.js
@@ -1,37 +1,21 @@
 'use strict';
 
-var mountFolder = function (connect, dir) {
-	return connect.static(require('path').resolve(dir));
-};
-
-module.exports = function (grunt, options) {
-	var config = require('./config.js');
-
-	return {
+module.exports = {
+	options: {
+		open: true
+	},
+	build: {
 		options: {
-			open: true
-		},
-		build: {
-			options: {
-				middleware: function (connect) {
-					return [
-						mountFolder(connect, config.build)
-					];
-				},
-				hostname: 'localhost',
-				livereload: true,
-				port: 9000
-			}
-		},
-		prod: {
-			options: {
-				keepalive: true,
-				middleware: function (connect) {
-					return [
-						mountFolder(connect, config.prod)
-					];
-				}
-			}
+			base: '<%= config.build %>',
+			hostname: 'localhost',
+			livereload: true,
+			port: 9000
 		}
-	};
+	},
+	prod: {
+		options: {
+			base: '<%= config.prod %>',
+			keepalive: true
+		}
+	}
 };
