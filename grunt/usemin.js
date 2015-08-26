b648df2963d89744ecc493caafa0dabb804f5df7
Christopher Parsons <cparsons@habaneroconsulting.com>
2015-08-26 09:59:36 -0700

    Added OpenGraph image, fixed filerev issue with OpenGraph

--- grunt/usemin.js
+++ grunt/usemin.js
@@ -10,8 +10,8 @@ module.exports = {
 			html_srcset: [
 				// Requires three attempts for 1x, 2x, 3x
 				[/srcset="([^, ]+),/g, 'Replacing reference to srcset images'],
-				[/srcset=".+, (.+) 2x/g, 'Replacing reference to 2x srcset images'],
-				[/srcset=".+, (.+) 3x"/g, 'Replacing reference to 3x srcset images']
+				[/srcset=".+, (.+) .x/g, 'Replacing reference to x srcset images'],
+				[/content=".+\/(.+)"/g, 'Replacing reference to meta content']
 			]
 		}
 	},
