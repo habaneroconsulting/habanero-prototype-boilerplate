c55901bf9d965cfcb20dbdf16be720c3043f1236
Christopher Parsons <cparsons@habaneroconsulting.com>
2016-08-25 13:05:48 -0400

    Added postcss using gulp

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,15 +1,24 @@
 'use strict';
 
+const autoprefixer = require('autoprefixer');
 const gulp = require('gulp');
+const pixrem = require('pixrem');
+const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 
 module.exports = (src, dest, opts) => {
 	opts = Object.assign({}, opts);
 
+	const processors = [
+		pixrem(),
+		autoprefixer()
+	];
+
 	return gulp.src(src)
 		.pipe(sourcemaps.init())
 		.pipe(sass(opts))
+		.pipe(postcss(processors))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(dest))
 		.on('error', console.log);
--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ const config = require('./gulp/config');
 const gulp = require('gulp');
 const jscs = require('./gulp/jscs');
 const jshint = require('./gulp/jshint');
-const sass = require('./gulp/sass');
 const sasslint = require('./gulp/sasslint');
+const styles = require('./gulp/styles');
 
 gulp.task('clean:temp', () => clean([config.dirs.temp]));
 gulp.task('clean:build', () => clean([config.dirs.build]));
@@ -42,15 +42,15 @@ gulp.task('test', ['jshint', 'jscs', 'sasslint']);
 gulp.task('assemble:build', () => assemble(`${config.dirs.pages}/${config.files.templates}`, config.dirs.build));
 gulp.task('assemble:production', () => assemble(`${config.dirs.pages}/${config.files.templates}`, config.dirs.production));
 
-gulp.task('sass:build', () =>
-	sass([
+gulp.task('styles:build', () =>
+	styles([
 		`${config.dirs.src}/${config.files.scss}`,
 		`!${config.dirs.src}/**/vendor/**/${config.files.scss}`
 	], config.dirs.build)
 );
 
-gulp.task('sass:production', () =>
-	sass([
+gulp.task('styles:production', () =>
+	styles([
 		`${config.dirs.src}/${config.files.scss}`,
 		`!${config.dirs.src}/**/vendor/**/${config.files.scss}`
 	], config.dirs.production)
